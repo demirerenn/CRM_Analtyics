dc500d922d5d618b109bdbdabf750c7a207b2365
demirerenn <79810818+demirerenn@users.noreply.github.com>
2022-07-23 21:45:14 +0300

    Update cltv.py

--- cltv.py
+++ cltv.py
@@ -9,9 +9,8 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 pd.set_option('display.width', 500)
 
-"""This is a transnational data set which contains all the transactions occurring between 01/12/2010 and 09/12/2011 
-for a UK-based and registered non-store online retail.The company mainly sells unique all-occasion gifts. Many 
-customers of the company are wholesalers. 
+"""This is a transnational data set which contains all the transactions occurring between 01/12/2010 and 09/12/2011 for a UK-based 
+and registered non-store online retail.The company mainly sells unique all-occasion gifts. Many customers of the company are wholesalers. 
 https://www.kaggle.com/datasets/carrie1/ecommerce-data
 """
 
